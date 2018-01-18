a0349ba03d132837e1ee0778488194fdcd4f8df4
Philip Chang <sgnoohc@gmail.com>
2018-01-17 17:11:37 -0800

    update

--- dilepbabymaker/ScanChain_v2.cc
+++ dilepbabymaker/ScanChain_v2.cc
@@ -1,94 +1,6 @@
-// C++
-#include <iostream>
-#include <vector>
-#include <set>
-#include <string>
-#include <vector>
-
-// ROOT
-#include "TChain.h"
-#include "TDirectory.h"
-#include "TTreeCache.h"
-#include "Math/VectorUtil.h"
-#include "TVector2.h"
-#include "TBenchmark.h"
-#include "TLorentzVector.h"
-#include "TH2.h"
-#include "TROOT.h"
-#include "TFile.h"
-#include "TChain.h"
-#include "TTree.h"
-#include "TH2.h"
-#include "TString.h"
-#include "TMVA/Reader.h"
-#include "Math/LorentzVector.h"
-#include "Math/GenVector/LorentzVector.h"
-
-// CORE
-#include "../CORE/CMS3.h"
-#include "../CORE/Base.h"
-#include "../CORE/OSSelections.h"
-#include "../CORE/SSSelections.h"
-#include "../CORE/VVVSelections.h"
-#include "../CORE/ElectronSelections.h"
-#include "../CORE/IsolationTools.h"
-#include "../CORE/JetSelections.h"
-#include "../CORE/MuonSelections.h"
-#include "../CORE/IsoTrackVeto.h"
-#include "../CORE/PhotonSelections.h"
-#include "../CORE/TriggerSelections.h"
-#include "../CORE/VertexSelections.h"
-#include "../CORE/MCSelections.h"
-#include "../CORE/MetSelections.h"
-#include "../CORE/SimPa.h"
-#include "../CORE/Tools/jetcorr/FactorizedJetCorrector.h"
-#include "../CORE/Tools/JetCorrector.h"
-#include "../CORE/Tools/jetcorr/JetCorrectionUncertainty.h"
-#include "../CORE/Tools/MT2/MT2.h"
-#include "../CORE/Tools/hemJet.h"
-#include "../CORE/Tools/utils.h"
-#include "../CORE/Tools/goodrun.h"
-#include "../CORE/Tools/btagsf/BTagCalibrationStandalone.h"
-#include "../CORE/Tools/btagsf/BTagCalibrationStandalone.h"
+#include "ScanChain_v2.h"
 
 using namespace std;
-
-typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > LorentzVector;
-
-
-// `````````````````````````````````````````````````````````````````````````````````````````````````````````````
-//
-//
-//
-// Baby Maker Class
-//
-//
-//
-// ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-
-class babyMaker_v2
-{
-
-private:
-
-    SimPa simpa;
-    const char* json_file;
-
-public:
-
-    babyMaker_v2() {};
-    ~babyMaker_v2() {};
-    void ScanChain_v2(TChain*, std::string = "testSample", int max_events = -1);
-    void Configure();
-    void ConfigureElectronMVA();
-    void ConfigureMuonMVA();
-    void ConfigurePileUpReweighting();
-    void ConfigureGoodRunsList();
-    void ConfigureBtagging();
-};
-
-// RooUtil
-#include "rooutil/looper.h"
 
 //##############################################################################################################
 void babyMaker_v2::ScanChain_v2(TChain* chain, std::string baby_name, int max_events)
@@ -101,6 +13,53 @@ void babyMaker_v2::ScanChain_v2(TChain* chain, std::string baby_name, int max_ev
 
     while (looper.nextEvent())
     {
+        isData = tas::evt_isRealData();
+
+        //======================================================
+        // Triggers
+        //======================================================
+        HLT_DoubleMu = (passHLTTriggerPattern("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v") ||
+                        passHLTTriggerPattern("HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v") ||
+                        passHLTTriggerPattern("HLT_TkMu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v") ||
+                        passHLTTriggerPattern("HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v") ||
+                        passHLTTriggerPattern("HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v"));
+
+        HLT_MuEG = (passHLTTriggerPattern("HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v") ||
+                    passHLTTriggerPattern("HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v") ||
+                    passHLTTriggerPattern("HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v") ||
+                    passHLTTriggerPattern("HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v") ||
+                    passHLTTriggerPattern("HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v") ||
+                    passHLTTriggerPattern("HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v") ||
+                    passHLTTriggerPattern("HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v") ||
+                    passHLTTriggerPattern("HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ_v") ||
+                    passHLTTriggerPattern("HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v"));
+
+        HLT_DoubleEl = (passHLTTriggerPattern("HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_v") ||
+                        passHLTTriggerPattern("HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v")); // prescaled - turned off
+
+        HLT_DoubleEl_DZ = passHLTTriggerPattern("HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v"); // prescaled
+
+        // Loop over gen particles
+
+        // Loop over electrons
+
+        // Loop over muons
+
+        // Organize leptons by sorting
+
+        // Photons Not needed
+
+        // Select based on two leptons
+
+        // Loop over jets (keep track of good ones for removal)
+
+        // jet lepton overlap
+
+        // Loop over good jets after removal and select
+
+        // kinematic variables for pf cands (isotrack)
+
+        coreJec.setJECFor(looper.getCurrentFileName());
     }
 }
 
@@ -199,3 +158,4 @@ void babyMaker_v2::ConfigureBtagging()
 //    // f_btag_eff->Close();
 //    std::cout << "loaded fullsim btag SFs" << std::endl;
 }
+
--- dilepbabymaker/ScanChain_v2.h
+++ dilepbabymaker/ScanChain_v2.h
@@ -1,5 +1,106 @@
-
 #ifndef ScanChain_h
 #define ScanChain_h
+
+// C++
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <vector>
+
+// ROOT
+#include "TChain.h"
+#include "TDirectory.h"
+#include "TTreeCache.h"
+#include "Math/VectorUtil.h"
+#include "TVector2.h"
+#include "TBenchmark.h"
+#include "TLorentzVector.h"
+#include "TH2.h"
+#include "TROOT.h"
+#include "TFile.h"
+#include "TChain.h"
+#include "TTree.h"
+#include "TH2.h"
+#include "TString.h"
+#include "TMVA/Reader.h"
+#include "Math/LorentzVector.h"
+#include "Math/GenVector/LorentzVector.h"
+
+// CORE
+#include "../CORE/CMS3.h"
+#include "../CORE/Base.h"
+#include "../CORE/OSSelections.h"
+#include "../CORE/SSSelections.h"
+#include "../CORE/VVVSelections.h"
+#include "../CORE/ElectronSelections.h"
+#include "../CORE/IsolationTools.h"
+#include "../CORE/JetSelections.h"
+#include "../CORE/MuonSelections.h"
+#include "../CORE/IsoTrackVeto.h"
+#include "../CORE/PhotonSelections.h"
+#include "../CORE/TriggerSelections.h"
+#include "../CORE/VertexSelections.h"
+#include "../CORE/MCSelections.h"
+#include "../CORE/MetSelections.h"
+#include "../CORE/SimPa.h"
+//#include "../CORE/Tools/jetcorr/FactorizedJetCorrector.h"
+//#include "../CORE/Tools/JetCorrector.h"
+//#include "../CORE/Tools/jetcorr/JetCorrectionUncertainty.h"
+#include "../CORE/Tools/MT2/MT2.h"
+#include "../CORE/Tools/hemJet.h"
+#include "../CORE/Tools/utils.h"
+#include "../CORE/Tools/goodrun.h"
+#include "../CORE/Tools/btagsf/BTagCalibrationStandalone.h"
+#include "../CORE/Tools/btagsf/BTagCalibrationStandalone.h"
+
+// RooUtil
+#include "rooutil/looper.h"
+
+// CoreUtil
+#include "coreutil/jec.h"
+
+typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > LorentzVector;
+
+// `````````````````````````````````````````````````````````````````````````````````````````````````````````````
+//
+//
+//
+// Baby Maker Class
+//
+//
+//
+// ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+class babyMaker_v2
+{
+
+private:
+
+    SimPa simpa;
+    const char* json_file;
+    CoreUtil::jec coreJec;
+
+    bool isData;
+
+    bool HLT_DoubleMu;
+    bool HLT_MuEG;
+    bool HLT_DoubleEl;
+    bool HLT_DoubleEl_DZ;
+
+public:
+
+    babyMaker_v2() {}
+    ~babyMaker_v2() {}
+    void ScanChain_v2(TChain*, std::string = "testSample", int max_events = -1);
+    void Configure();
+    void ConfigureElectronMVA();
+    void ConfigureMuonMVA();
+    void ConfigurePileUpReweighting();
+    void ConfigureGoodRunsList();
+    void ConfigureBtagging();
+    void ConfigureJEC();
+};
+
 
 #endif
