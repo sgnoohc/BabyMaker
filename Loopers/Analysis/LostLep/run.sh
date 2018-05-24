0a08509a5e9210d82b05b17118564d5a319fb570
Philip Chang <sgnoohc@gmail.com>
2018-05-24 14:08:39 -0700

    add plotting step

--- Loopers/Analysis/LostLep/run.sh
+++ Loopers/Analysis/LostLep/run.sh
@@ -11,3 +11,6 @@ python maketable.py output_jec_up.root jec_up
 python maketable.py output_jec_dn.root jec_dn
 
 # each cutflow table in cutflows contain the alpha factor between N_lostlep,SR / N_lostlep,CR
+
+# run plotter
+python makeplot.py
