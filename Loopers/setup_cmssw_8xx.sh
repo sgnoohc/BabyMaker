2b2c7f21565bfbd40e7f84bcfbfb5ecdef445ee9
Philip Chang <sgnoohc@gmail.com>
2018-08-07 17:54:07 -0700

    delete json looper

--- Loopers/setup_cmssw_8xx.sh
+++
@@ -1,6 +0,0 @@
-CMSSW_VERSION=CMSSW_8_0_5
-export SCRAM_ARCH=slc6_amd64_gcc530
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/cmssw/$CMSSW_VERSION/src
-eval `scramv1 runtime -sh`
-cd -
